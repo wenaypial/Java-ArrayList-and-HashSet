647da5bb02801c4413c2bb47a23cedc073879b43
wenaypial <21103866@usc.edu.ph>
2022-09-26 15:21:23 +0800

    Added A code

---
+++ ArrayList_HashSet/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+
+        System.out.println(numbers.toString());
+        System.out.println(numbers);
+    }
+}
